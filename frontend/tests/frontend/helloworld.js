71c7fd81d9b9821c282698e96aa8c6d32eb0e948
Jean Cochrane <jean@jeancochrane.com>
2019-03-20 10:22:51 -0400

    Add comments to frontend dev environment for clarity

--- frontend/tests/frontend/helloworld.js
+++ frontend/tests/frontend/helloworld.js
@@ -1,5 +1,7 @@
 import { helloWorld } from "../../src/js/helloworld";
 
+// Instruct JSHint to ignore the Jasmine global objects (e.g. 'describe', 'it')
+// that appear to be undefined.
 /* jshint undef: false */
 describe("Test helloWorld", function() {
   it("returns true", function () {
