eedcf8cefb600d89d5ab8565e8b43fe2fff4cc71
Bea Malsky <beamalsky@gmail.com>
2019-04-03 15:53:43 -0500

    Delete comments

--- tests/test_data_handling.py
+++ tests/test_data_handling.py
@@ -10,50 +10,44 @@ from pldp.models import Survey, SurveyRow, SurveyComponent
 
 @pytest.mark.django_db
 def test_data_handler(form_entry, location, study, form_element, mocker):
+    """Tests the custom Fobi data handler by instantiating a sample
+    survey, surveyrow, and surveycomponent with saved data"""
+
     component_data = json.loads(form_element.plugin_data)
     saved_data = 5
 
     request = mocker.MagicMock(spec=requests.Response)
     form_class = assemble_form_class(form_entry=form_entry)
 
-    # Instantiate an instance of the `assemble_form_class`.
     form_class = assemble_form_class(form_entry=form_entry)
     form = form_class(data={component_data['name']: saved_data})
 
     assert form.is_valid()
 
-    # A valid CollectDataPlugin requires a DynamicForm, a FormEntry, and a post request
     plugin = CollectDataPlugin()
     plugin.run(form=form,
                form_entry=form_entry,
                request=request)
 
-    # Check that a survey has been created with a linked row and component
     survey = Survey.objects.first()
     rows = SurveyRow.objects.filter(survey=survey)
 
-    # Grab the row object itself out of the filtered row queryset, in order to
-    # find its linked components
     row = rows[0]
     components = SurveyComponent.objects.filter(row=row)
     component = components[0]
 
-    # Check that the handler has created a single row and linked component
     assert len(rows) == 1
     assert len(components) == 1
 
-    # Check survey has the correct values
     assert survey.id
     assert survey.time_stop
     assert survey.location == location
     assert survey.study == study
 
-    # Check row has the correct values
     assert row.id
     assert row.total
     assert row.survey == survey
 
-    # Check component has the correct values
     assert component.id
     assert component.detail_level == 'basic'
     assert component.name == component_data['name']
