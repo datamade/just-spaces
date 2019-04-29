492160e19d323bf7c3382586353af0f42638fb25
Bea Malsky <beamalsky@gmail.com>
2019-04-29 16:48:52 -0500

    PR edits

--- fobi_custom/plugins/form_elements/fields/observational/fields.py
+++ fobi_custom/plugins/form_elements/fields/observational/fields.py
@@ -1,14 +1,15 @@
+import collections
 from django import forms
 
 
 class ObservationalField(forms.MultiValueField):
     def __init__(self, choices=None, *args, **kwargs):
-        fields = [forms.IntegerField(label="ldkfjs")] * len(choices)
+        fields = [forms.IntegerField()] * len(choices)
         self.choices = choices
         super(ObservationalField, self).__init__(fields, *args, **kwargs)
 
     def compress(self, data_list):
         choices_cleaned = [choice[0] for choice in self.choices]
-        saved_data = list(zip(choices_cleaned, data_list))
+        saved_data = collections.OrderedDict(zip(choices_cleaned, data_list))
 
         return saved_data
