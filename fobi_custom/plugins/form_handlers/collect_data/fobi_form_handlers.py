d560ae737dab8bcfc4d296aba421c9e6791821ec
Bea Malsky <beamalsky@gmail.com>
2019-05-01 16:20:55 -0500

    Delete comments

--- fobi_custom/plugins/form_handlers/collect_data/fobi_form_handlers.py
+++ fobi_custom/plugins/form_handlers/collect_data/fobi_form_handlers.py
@@ -45,11 +45,8 @@ class CollectDataPlugin(FormHandlerPlugin):
 
             new_survey = Survey.objects.create(**new_survey_info)
 
-            # total = self.get_saved_data('total', 1)
-
             new_survey_row = SurveyRow.objects.create(
                 survey=new_survey,
-                # total=total
             )
 
             meta_element_names = [name for (name, default) in meta_elements]
