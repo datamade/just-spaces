760eb302a14efde981f4375ecc6414a6850afd6c
Bea Malsky <beamalsky@gmail.com>
2019-05-01 10:52:10 -0500

    Add dropdown choics

--- fobi_custom/plugins/form_elements/fields/intercept/forms.py
+++ fobi_custom/plugins/form_elements/fields/intercept/forms.py
@@ -8,22 +8,50 @@ GENDER_INTERCEPT_CHOICES = [
 
 INCOME_CHOICES = [
     ('', '-------'),
-    ('tk', 'TK'),
+    ('<20k', 'Less than $20,000 per year'),
+    ('20-40k', '$20,000-$40,000 per year'),
+    ('40-60k', '$40,000-$60,000 per year'),
+    ('60-80k', '$60,000-$80,000 per year'),
+    ('80-100k', '$80,000-$100,000 per year'),
+    ('100k+', 'More than $100,000 per year'),
 ]
 
 EDUCATION_CHOICES = [
     ('', '-------'),
-    ('tk', 'TK'),
+    ('no_high_school', 'No high school diploma'),
+    ('high_school', 'High school diploma'),
+    ('associate', 'Associate\'s degree'),
+    ('bachelor', 'Bachelor\'s degree'),
+    ('graduate', 'Graduate/professional degree')
 ]
 
 RACE_CHOICES = [
     ('', '-------'),
-    ('tk', 'TK'),
+    ('black', 'African-American/Black'),
+    ('asian', 'Asian'),
+    ('white', 'White'),
+    ('hispanic_latino', 'Hispanic or Latino'),
+    ('native', 'Native American or Alaska Native'),
+    ('hawaiian', 'Native Hawaiian or Pacific Islander'),
+    ('multiple', 'Two or more races'),
+    ('other', 'Other'),
 ]
 
 EMPLOYMENT_CHOICES = [
     ('', '-------'),
-    ('tk', 'TK'),
+    ('full_time', 'Working full-time'),
+    ('part_time', 'Working part-time'),
+    ('seeking', 'Seeking work'),
+    ('not_seeking', 'Not seeking work'),
+    ('student', 'Student'),
+    ('homemaker', 'Homemaker'),
+]
+
+OWN_OR_RENT_CHOICES = [
+    ('', '-------'),
+    ('owner', 'Homeowner'),
+    ('renter', 'Renter'),
+    ('other', 'Other'),
 ]
 
 TRANSPORTATION_CHOICES = [
