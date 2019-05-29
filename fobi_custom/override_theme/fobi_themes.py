b06167130a72b353913ff47bb59d7ee6d0d0bcf2
Bea Malsky <beamalsky@gmail.com>
2019-05-29 18:54:07 -0500

    Cancel buttons on edit and add form element views

--- fobi_custom/override_theme/fobi_themes.py
+++ fobi_custom/override_theme/fobi_themes.py
@@ -12,10 +12,10 @@ class JustSpacesTheme(Foundation5Theme):
     base_template = 'override_theme/base.html'
     edit_form_entry_ajax_template = 'override_theme/edit_form_entry_ajax.html'
     form_snippet_template_name = 'override_theme/snippets/form_snippet.html'
-    edit_form_element_entry_template = 'override_theme/edit_form_element_entry.html'
     form_ajax = 'override_theme/snippets/form_ajax.html'
     form_entry_submitted_ajax_template = 'override_theme/form_entry_submitted_ajax.html'
     add_form_element_entry_ajax_template = 'override_theme/add_form_element_entry_ajax.html'
+    edit_form_element_entry_ajax_template = 'override_theme/edit_form_element_entry_ajax.html'
 
 # It's important to set the `force` argument to True, in
 # order to override the original theme. Force can be applied
