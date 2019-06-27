e118782aee226e546bdc1e1d4f1de22efff2948c
Bea Malsky <beamalsky@gmail.com>
2019-06-27 15:03:10 -0500

    Modify user help text on admin site

--- users/admin.py
+++ users/admin.py
@@ -21,18 +21,21 @@ class JustSpacesUserChangeForm(UserChangeForm):
 
 
 class JustSpacesUserAdmin(UserAdmin):
-    permissions_description = 'Just Spaces has three levels of user \
-        permissions:<br /><br /> \
-        1. <b>Field users</b> can run published surveys.<br /> \
-        2. <b>Staff users</b> have all the permissions of field users. They can \
-            also create, edit, and delete Agencies, Studies, Study Areas, \
-            Surveys, and Locations. They can publish surveys so they can \
-            be run by field users. Staff users can also view all collected \
-            data and design data visualizations on the collected data pages.<br /> \
-        3. <b>Superusers</b> have all the permissions of staff users. They can \
-            also create, edit, and delete other users. Only superusers can set \
-            and change user passwords. <b>All superusers should \
-            also have "Staff status" selected below</b>.<br />'
+    permissions_description = 'This app employs three classes of \
+        users:<br /><br /> \
+        1. <b>Field users</b> can run published surveys. <b>To create a \
+            field user, check \'Active\' below.</b><br /> \
+        2. <b>Staff users</b> have all the permissions of field users. They \
+            can also create, edit, and delete Agencies, Studies, Study Areas, \
+            Surveys, and Locations. They can publish surveys so they can be \
+            run by field users. Staff users can also view all collected data \
+            and design data visualizations on the collected data pages. <b>To \
+            create a staff user, check \'Active\' and \'Staff status\' \
+            below.</b><br /> \
+        3. <b>Superusers</b> have all the permissions of staff users. They \
+            can also create, edit, and delete other users. Only superusers \
+            can set and change user passwords. <b>To create a superuser, \
+            check all the boxes below.</b>'
 
     fieldsets = (
         (None, {'fields': ('username', 'password', 'agency')}),
