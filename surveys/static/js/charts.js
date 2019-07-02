2bed8f08b0ad9bbd3cfbe2b31ab9630357f2ea6c
Bea Malsky <beamalsky@gmail.com>
2019-07-02 17:06:49 -0500

    let -> var

--- surveys/static/js/charts.js
+++ surveys/static/js/charts.js
@@ -540,7 +540,7 @@ function binValue(value, bins) {
  * @param {Array} a items An array containing the items.
  */
 function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
+    for (var i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [a[i], a[j]] = [a[j], a[i]];
     }
