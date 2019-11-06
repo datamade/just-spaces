fddd71aaf0e2a0693b410494dced63703ae48ca0
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2019-11-06 13:13:19 -0600

    Add new regions to data import

--- data/shapefiles.mk
+++ data/shapefiles.mk
@@ -8,7 +8,22 @@ shapefiles: final/shapefiles/cb_2018_42_bg_500k.shp \
             final/shapefiles/cb_2018_45_bg_500k.shp \
             final/shapefiles/cb_2018_17_bg_500k.shp \
             final/shapefiles/cb_2018_18_bg_500k.shp \
-            final/shapefiles/cb_2018_55_bg_500k.shp
+            final/shapefiles/cb_2018_55_bg_500k.shp \
+            final/shapefiles/cb_2018_48_bg_500k.shp \
+            final/shapefiles/cb_2018_26_bg_500k.shp \
+            final/shapefiles/cb_2018_29_bg_500k.shp \
+            final/shapefiles/cb_2018_20_bg_500k.shp \
+            final/shapefiles/cb_2018_06_bg_500k.shp \
+            final/shapefiles/cb_2018_21_bg_500k.shp \
+            final/shapefiles/cb_2018_12_bg_500k.shp \
+            final/shapefiles/cb_2018_27_bg_500k.shp \
+            final/shapefiles/cb_2018_34_bg_500k.shp \
+            final/shapefiles/cb_2018_36_bg_500k.shp \
+            final/shapefiles/cb_2018_53_bg_500k.shp \
+            final/shapefiles/cb_2018_11_bg_500k.shp \
+            final/shapefiles/cb_2018_24_bg_500k.shp \
+            final/shapefiles/cb_2018_51_bg_500k.shp \
+            final/shapefiles/cb_2018_54_bg_500k.shp
 
 raw/shapefiles/cb_2018_%_bg_500k.zip:
 	wget -O $@ https://www2.census.gov/geo/tiger/GENZ2018/shp/$(notdir $@)
